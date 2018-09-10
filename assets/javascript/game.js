d7b7be1c1321a5ce069eab555a159e442d74cccf
Vu Le <thuy.dtt1118@Thuys-MacBook-Air.local>
2018-09-10 18:25:23 -0500

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,68 +4,52 @@ $(document).ready(function () {
 	$("#required").html(required);
 
 	var potion1 = Math.floor(Math.random() * 12) + 1;
-	$("#obj1").attr("value", potion1)
-	console.log(potion1)
+	$("#obj1").attr("value", potion1);
+	console.log(potion1);
 
 	var potion2 = Math.floor(Math.random() * 12) + 1;
-	$("#obj2").attr("value", potion2)
-	console.log(potion2)
+	$("#obj2").attr("value", potion2);
+	console.log(potion2);
 
 	var potion3 = Math.floor(Math.random() * 12) + 1;
-	$("#obj3").attr("value", potion3)
-	console.log(potion3)
+	$("#obj3").attr("value", potion3);
+	console.log(potion3);
 
 	var potion4 = Math.floor(Math.random() * 12) + 1;
-	$("#obj4").attr("value", potion4)
-	console.log(potion4)
+	$("#obj4").attr("value", potion4);
+	console.log(potion4);
 
 	var demons = 0;
 
 	var djts = 0;
 
 	var score = 0;
+	$("#player").html(score);
 
-	function reset() {
-		var required = Math.floor(Math.random() * 102) + 19;
+	function reset() {		
+
+		required = Math.floor(Math.random() * 102) + 19;
 		$("#required").html(required);
 
 		score = 0;
 		$("#player").html(score);
 
+		potion1 = Math.floor(Math.random() * 12) + 1;
+		$("#obj1").attr("value", potion1);
+		console.log(potion1);
 
-		var potion1 = Math.floor(Math.random() * 12) + 1;
-		$("#obj1").attr("value", potion1)
-		console.log(potion1)
+		potion2 = Math.floor(Math.random() * 12) + 1;
+		$("#obj2").attr("value", potion2);
+		console.log(potion2);
 
-		var potion2 = Math.floor(Math.random() * 12) + 1;
-		$("#obj2").attr("value", potion2)
-		console.log(potion2)
+		potion3 = Math.floor(Math.random() * 12) + 1;
+		$("#obj3").attr("value", potion3);
+		console.log(potion3);
 
-		var potion3 = Math.floor(Math.random() * 12) + 1;
-		$("#obj3").attr("value", potion3)
-		console.log(potion3)
+		potion4 = Math.floor(Math.random() * 12) + 1;
+		$("#obj4").attr("value", potion4);
+		console.log(potion4);
 
-		var potion4 = Math.floor(Math.random() * 12) + 1;
-		$("#obj4").attr("value", potion4)
-		console.log(potion4)
-
-		
-		$("img").on("click", function () {
-			var total = score += parseInt($(this).attr("value"));
-			$("#player").html(total);
-
-			if (total === required) {
-				demons++;
-				$("#demon").html(demons);
-				reset();
-			}
-
-			else if (total > required) {
-				djts++;
-				$("#djt").html(djts);
-				reset();
-			}
-		});
 	}
 
 	$("img").on("click", function () {
